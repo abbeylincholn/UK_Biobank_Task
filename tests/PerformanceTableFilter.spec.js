e9a2372444a9a4e3f5cbf62ab20706f81bcdf7dd
abbey <abbeyezekiel100@gmail.com>
2025-09-28 17:24:20 +0000

    update1

--- tests/PerformanceTableFilter.spec.js
+++ tests/PerformanceTableFilter.spec.js
@@ -1,37 +1,21 @@
-// const {test, expect} = require('@playwright/test')
-// const {POManager} = require('../pageobject/POManager')
-// const data = require('../utils/TestData.json');
-
-
-// test('AG performance Filter', async ({ page }) => {
-
-//     const poManager = new POManager(page);
-
-//     const performance = poManager.getPerformancePage();
-//     await performance.goto();   
-//     await performance.sortByName(); 
-//     await performance.filterByLanguage(data.languages);
-//     await performance.filterByCountry(data.countries);
-//     await performance.filterByBought(true);  
-//     await performance.filterByTotalWinnings(300000, 1000000);         
-    
-//     //await page.pause()
-
-    
-// });
-
-const { test } = require('@playwright/test');
-const { POManager } = require('../pageobject/POManager');
+const {test, expect} = require('@playwright/test')
+const {POManager} = require('../pageobject/POManager')
 const data = require('../utils/TestData.json');
 
-test('AG performance Filter', async ({ page }) => {
-  const poManager = new POManager(page);
-  const performance = poManager.getPerformancePage();
 
-  await performance.goto();
-  await performance.sortByName();
-  await performance.filterByLanguage(data.languages);
-  await performance.filterByCountry(data.countries);
-  await performance.filterByBought(true);
-  await performance.filterByTotalWinnings(300000, 1000000);
+test('AG performance Filter', async ({ page }) => {
+
+    const poManager = new POManager(page);
+
+    const performance = poManager.getPerformancePage();
+    await performance.goto();   
+    await performance.sortByName(); 
+    await performance.filterByLanguage(data.languages);
+    await performance.filterByCountry(data.countries);
+    await performance.filterByBought(true);  
+    await performance.filterByTotalWinnings(300000, 1000000);         
+    
+    //await page.pause()
+
+    
 });
