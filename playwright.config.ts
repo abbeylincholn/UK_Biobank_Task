b8a96308cc35408f3f2b4672a3db9ab28fbf51cd
abbey <abbeyezekiel100@gmail.com>
2025-09-28 17:57:55 +0000

    up

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   reporter: [['line'], ['github'], ['html'], ['allure-playwright']],
   projects: [    
     //{ name: 'firefox', use: { browserName: 'firefox',  headless: true, trace: 'on', screenshot: 'only-on-failure', video: 'retain-on-failure' } },
-   // { name: 'safari',  use: { browserName: 'webkit',   headless: true, trace: 'on', screenshot: 'only-on-failure', video: 'retain-on-failure' } },
-   { name: 'chrome',  use: { browserName: 'chromium',   headless: true, trace: 'on', screenshot: 'only-on-failure', video: 'retain-on-failure' } },
+    { name: 'safari',  use: { browserName: 'webkit',   headless: true, trace: 'on', screenshot: 'only-on-failure', video: 'retain-on-failure' } },
+   //{ name: 'chrome',  use: { browserName: 'chromium',   headless: true, trace: 'on', screenshot: 'only-on-failure', video: 'retain-on-failure' } },
   ],
 });
